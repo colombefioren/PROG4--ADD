b3c30dca9768810fd534819526d2af3a242a2069
colombefioren <colomberakotonjanahary@gmail.com>
2026-05-26 09:20:24 +0300

    refactor: catch errors

--- src/main/java/org/coco/add/endpoint/rest/controller/add/AddController.java
+++ src/main/java/org/coco/add/endpoint/rest/controller/add/AddController.java
@@ -1,6 +1,8 @@
 package org.coco.add.endpoint.rest.controller.add;
 
 import org.coco.add.service.AddService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +15,11 @@ public class AddController {
   private AddService addService;
 
   @GetMapping
-  public int add(@RequestParam String a, @RequestParam String b) {
-    return addService.add(a, b);
+  public ResponseEntity<?> add(@RequestParam String a, @RequestParam String b) {
+    try{
+      return ResponseEntity.ok(addService.add(a, b));
+    }catch (IllegalArgumentException e){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
   }
 }
